e9b95365eb6ba30e3c269e365713269a92d673c9
Shafekul Abid <abidshafee@gmail.com>
2019-12-24 01:45:08 +0600

    call pl/sql

--- src/javaOracleConnectivity.java
+++ src/javaOracleConnectivity.java
@@ -7,6 +7,7 @@ public class javaOracleConnectivity {
             //load driver (optional!)
             //Class.forName("oracle.jdbc.driver.OracleDriver");
 
+            //ConnectionString
             Connection con = DriverManager.getConnection(
                     "jdbc:oracle:thin:abiddb/abid@//localhost:1521/orcl");
                         //here 'orcl' is the service name, 'thin' is the driver,
@@ -24,16 +25,20 @@ public class javaOracleConnectivity {
             //inserting value to student table
             String insertSql = "insert into student values (?,?,?,?)";
             PreparedStatement prstmt = con.prepareStatement(insertSql);
-            prstmt.setString(1,"16-0-52-020-061");
-            prstmt.setString(2, "Sojib Ahmed");
-            prstmt.setString(3, "eee");
-            prstmt.setFloat(4, (float) 138.5);
+            prstmt.setString(1,"16-0-52-020-065");
+            prstmt.setString(2, "unknown2 folk");
+            prstmt.setString(3, "cse");
+            prstmt.setFloat(4, (float) 139.5);
             prstmt.executeQuery();
 
+/*          //Calling PL/SQL in java
+            CallableStatement myCall = con.prepareCall("{CALL insertingTrigger()}");
+            myCall.execute();
 
+            String triggRes = myCall.getString("");
+            System.out.println(triggRes);
 
-
-
+*/
             //close the connection object
             con.close();
         }catch (SQLException sq){
@@ -43,5 +48,5 @@ public class javaOracleConnectivity {
 
     }
 
-    //CallableStatement myCall =
+
 }
