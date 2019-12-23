21e8bad44c66c695ea78e21706ee73a802670b25
Shafekul Abid <abidshafee@gmail.com>
2019-12-24 02:29:13 +0600

    call pl/sql

--- src/javaOracleConnectivity.java
+++ src/javaOracleConnectivity.java
@@ -25,28 +25,43 @@ public class javaOracleConnectivity {
             //inserting value to student table
             String insertSql = "insert into student values (?,?,?,?)";
             PreparedStatement prstmt = con.prepareStatement(insertSql);
-            prstmt.setString(1,"16-0-52-020-065");
-            prstmt.setString(2, "unknown2 folk");
+            prstmt.setString(1,"16-0-52-020-01");
+            prstmt.setString(2, "Labiba Yasmin");
             prstmt.setString(3, "cse");
-            prstmt.setFloat(4, (float) 139.5);
+            prstmt.setFloat(4, (float) 149.5);
             prstmt.executeQuery();
 
-/*          //Calling PL/SQL in java
-            CallableStatement myCall = con.prepareCall("{CALL insertingTrigger()}");
+            /*
+                PL/SQL Trigger: on Student Table
+                _______________
+                SET SERVEROUTPUT ON;
+                  create trigger insertingTrigger
+                    before INSERT ON Student
+                        for each ROW
+                    enable
+                DECLARE
+                    myVar VARCHAR(20);
+                BEGIN
+                    SELECT user into myVar FROM dual;
+                    DBMS_OUTPUT.PUT_LINE('A new row inserted by '||myVar);
+                end;
+                /
+            */
+
+          //Calling PL/SQL in java
+            CallableStatement myCall = con.prepareCall("{CALL insertingTrigger}");
             myCall.execute();
 
-            String triggRes = myCall.getString("");
-            System.out.println(triggRes);
+            //String triggRes = myCall.getString("");
+            System.out.println(psRes);
 
-*/
+
             //close the connection object
             con.close();
         }catch (SQLException sq){
             System.err.println(sq.getMessage());
-        }
+        }//endOfTryCatch
 
 
-    }
-
-
+    }//end main
 }
